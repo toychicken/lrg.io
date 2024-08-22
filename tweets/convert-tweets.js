e847259eb0a6f553650473737930a16137000fc4
Leigh Garland <leigh@toychicken.co.uk>
2024-08-22 13:39:42 +0100

    Source of all the old tweets

---
+++ tweets/convert-tweets.js
@@ -0,0 +1,72 @@
+const leighs = require('./tweets.js')
+const moment = require('moment')
+const matter = require('gray-matter')
+const fs = require('fs')
+
+for (t of leighs.tweets) {
+  
+  //ignore any replies or old-style RT
+  if(t.tweet.in_reply_to_user_id_str === undefined && !t.tweet.full_text.startsWith('RT @')) {
+
+    //does this tweet have media?
+    let allMedia = []
+    if(t.tweet.entities.media) {
+      for (media of t.tweet.entities.media) {
+        let file = media.media_url.substring(media.media_url.lastIndexOf('/') + 1)
+        allMedia.push({ url: t.tweet.id + '-' + file })
+      }
+    }
+
+    //does this tweet have links?
+    let allLinks = []
+    if(t.tweet.entities.urls) {
+      for (url of t.tweet.entities.urls) {
+        allLinks.push(url.expanded_url)
+      }
+    }
+
+    //does this tweet mention a user?
+    let allMentions = []
+    if(t.tweet.entities.user_mentions) {
+      for(mention of t.tweet.entities.user_mentions) {
+        allMentions.push(`@${mention.screen_name}`)
+      }
+    }
+
+    //does this tweet have hashtags?
+    let allHashTags = []
+    if(t.tweet.entities.hashtags) {
+      for(tag of t.tweet.entities.hashtags) {
+        allHashTags.push(tag.text)
+      }
+    }
+
+    const photos = allMedia.length === 0 ? {} : { photo: allMedia }
+    const links = allLinks.length === 0 ? {} : { links: allLinks }
+    const mentions = allMentions.length === 0 ? {} : { mentions: allMentions }
+    const tags = allHashTags.length === 0 ? {} : { tags: allHashTags }
+
+    //remove any links
+    let fileText = t.tweet.full_text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')
+    //replace @mentions with linked mentioned
+    for (mention of allMentions) {
+      fileText = fileText.replace(new RegExp(mention, "gi"), `[${mention}](https://twitter.com/${mention})`)
+    }
+
+    const description = {description: `"${fileText}"`};
+
+    let fileContent = matter.stringify(fileText, {
+      title: "Tweet",
+      ...description,
+      date: moment(t.tweet.created_at).format('YYYY-MM-DDTHH:MM:ssZ'),
+      ...photos,
+      ...links,
+      ...mentions,
+      ...tags,
+      
+    })
+    fs.writeFileSync(`../content/posts/${moment(t.tweet.created_at).format('YYYY-MM-DD')}-${t.tweet.id}.md`, fileContent)
+  }
+
+  
+}
---
+++ tweets/tewts.js
@@ -0,0 +1,94 @@
+module.exports = { tweets: [
+    {
+      "tweet" : {
+        "edit_info" : {
+          "initial" : {
+            "editTweetIds" : [
+              "1632897735476011010"
+            ],
+            "editableUntil" : "2023-03-07T00:45:32.000Z",
+            "editsRemaining" : "5",
+            "isEditEligible" : true
+          }
+        },
+        "retweeted" : false,
+        "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+        "entities" : {
+          "hashtags" : [ ],
+          "symbols" : [ ],
+          "user_mentions" : [ ],
+          "urls" : [ ]
+        },
+        "display_text_range" : [
+          "0",
+          "109"
+        ],
+        "favorite_count" : "1",
+        "id_str" : "1632897735476011010",
+        "truncated" : false,
+        "retweet_count" : "0",
+        "id" : "1632897735476011010",
+        "created_at" : "Tue Mar 07 00:15:32 +0000 2023",
+        "favorited" : false,
+        "full_text" : "Huh, looks like there's still people here. \nDon't worry about me, I'm just here to collect some stuff I left.",
+        "lang" : "en"
+      }
+    },
+    {
+      "tweet" : {
+        "edit_info" : {
+          "initial" : {
+            "editTweetIds" : [
+              "1603552212445265920"
+            ],
+            "editableUntil" : "2022-12-16T01:16:54.000Z",
+            "editsRemaining" : "5",
+            "isEditEligible" : true
+          }
+        },
+        "retweeted" : false,
+        "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+        "entities" : {
+          "hashtags" : [ ],
+          "symbols" : [ ],
+          "user_mentions" : [
+            {
+              "name" : "It's Going Down",
+              "screen_name" : "IGD_News",
+              "indices" : [
+                "26",
+                "35"
+              ],
+              "id_str" : "3289440085",
+              "id" : "3289440085"
+            },
+            {
+              "name" : "Mastodon (@Mastodon@mastodon.social)",
+              "screen_name" : "joinmastodon",
+              "indices" : [
+                "40",
+                "53"
+              ],
+              "id_str" : "875882643614814208",
+              "id" : "875882643614814208"
+            }
+          ],
+          "urls" : [ ]
+        },
+        "display_text_range" : [
+          "0",
+          "143"
+        ],
+        "favorite_count" : "0",
+        "id_str" : "1603552212445265920",
+        "truncated" : false,
+        "retweet_count" : "0",
+        "id" : "1603552212445265920",
+        "created_at" : "Fri Dec 16 00:46:54 +0000 2022",
+        "favorited" : false,
+        "full_text" : "Wow, E1on Müsk now banned @igd_news and @joinmastodon for... Reasons?\n\nLast call for those not wanting to support a fash website? No? Bye then.",
+        "lang" : "en"
+      }
+    }
+]
+}
