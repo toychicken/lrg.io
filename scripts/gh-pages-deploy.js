03d732a39c82ed150a6dc51a06e37e14a74d1900
Leigh Garland <leigh@toychicken.co.uk>
2024-10-04 16:48:24 +0100

    script updates

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -2,16 +2,23 @@ import { execa } from 'execa';
 const folderName = 'docs';
 const buildFolder = 'public' // should be same as publishDir in /config.yaml 
 const pagesBranch =  'gh-pages';
+const nixThese = ['_old_content', 'archetypes', 'content', 'static', 'themes', 'tweets'];
 
 // note! This puts the generated folder at the root of the directory in this working copy of the repo
 
 (async () => {
+
+  const nixy = async (xfolder) => {
+    await execa("rm", ["-r", xfolder])
+  }
+
   try {
     await execa("git", ["checkout", "--orphan", pagesBranch]);
     console.log("Building...");
     // build into the dist folder (folderName)
     await execa("hugo");
     await execa("mv", [`${buildFolder}`, `${folderName}`]);
+    nixThese.forEach(nixy)
     console.log("Built and moved to 'docs'");
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", pagesBranch]);
