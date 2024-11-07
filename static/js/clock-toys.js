84f11f080d0c4d1a367cd97487fe32918fb34d26
Leigh Garland <leigh@toychicken.co.uk>
2024-11-07 11:48:55 +0000

    Adding hexclock 2.0

--- static/js/clock-toys.js
+++ static/js/clock-toys.js
@@ -86,3 +86,21 @@ const timeToOtherThings = (t, is24hour = false) => {
         }
     }
 }
+
+const hslToHex = (h, s, l) => {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
+
+
+  const crudeDate = (offset) => {
+    let x = new Date();
+    x.setTime(x.getTime() + (offset * 60 * 60 * 1000));
+    return x;
+  }
